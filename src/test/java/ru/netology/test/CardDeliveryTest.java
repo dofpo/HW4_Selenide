bba67c41892b485b7df1c01d4a65fd83935c4786
dofpo <z-salawat91@mail.ru>
2022-07-31 17:51:38 +0500

    initial CardDeliveryTest commit

--- src/test/java/ru/netology/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -1,18 +1,27 @@
 package ru.netology.test;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @Test
     void test() {
+        String planningDate = generateDate(4);
         open("http://localhost:9999/");
         $("[data-test-id='city'] input").setValue("Уфа");
-        $x("//input[@placeholder='Дата встречи']").setValue("30.07.2022");
+        $x("//input[@placeholder='Дата встречи']").setValue(planningDate);
         $x("//input[@name='name']").setValue("Иванов Иван");
         $x("//input[@name='phone']").setValue("+78003000600");
         $("[data-test-id='agreement']").click();
@@ -20,4 +29,18 @@ public class CardDeliveryTest {
         $x("//*[contains(text(),'Успешно!')]").shouldBe(visible, Duration.ofSeconds(15));
     }
 
+    @Test
+    void test1() {
+        String planningDate = generateDate(4);
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Уфа");
+        $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.BACK_SPACE));
+        $x("//input[@placeholder='Дата встречи']").setValue(planningDate);
+        $x("//input[@name='name']").setValue("Иванов Иван");
+        $x("//input[@name='phone']").setValue("+78003000600");
+        $("[data-test-id='agreement']").click();
+        $x("//span[@class='button__text']").click();
+        $x("//*[contains(text(),'Успешно!')]").shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15)).shouldBe(visible);
+    }
 }
